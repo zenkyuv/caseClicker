96a259590f433efff37432d9dc992da3f3473954
Przemek Gałęzki <przemekgg2002@gmail.com>
2022-08-26 01:26:38 +0200

    add: main component

--- s/main.tsx
+++ s/main.tsx
@@ -1,11 +1,25 @@
-import Mainpage from './components/mainpage';
-import { StoresProvider, stores } from "./states-store/store";
-import "./styles/rootStyles.ts"
-const Main = () => {
+import { styles } from "./styles/mainpageStyles"
+import Sidenav from "./components/sidenav";
+import Inventory from "./components/inventory";
+import { useContext } from "react";
+import PageStore  from "./states-store/states/pageStore";
+import { observer } from "mobx-react-lite";
+import { ImageBackground, View } from "react-native";
+import ProfileBar from "./components/profilebar";
+import Clicker from "./components/caseOpener/clicker/clicker";
+
+const Mainpage = observer(() => {
+const pageStore = useContext(PageStore);
 	return (
-		<StoresProvider value={stores}>
-			<Mainpage/>
-		</StoresProvider>
-	)
-}
-export default Main
+		<View style={styles.container}>
+				<ImageBackground source={require("./images/panorama.jpg")} style={styles.image}>
+				<Sidenav />
+				{pageStore.inventoryVisible
+					? <Inventory />
+					: pageStore.clickerVisible
+					? <Clicker /> : null}
+				<ProfileBar />
+			</ImageBackground>
+			</View>
+			)})
+export default Mainpage;
