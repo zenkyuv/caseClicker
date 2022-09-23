157304a3967b996948a9b8d3babb011513cbacee
przemek <przemekgg2002@gmail.com>
2022-09-24 00:57:04 +0200

    refactor: simplify, cleanup, split getSkinRarityFrontend function, useComponentSize hook, case opener animation

--- s/components/caseOpener/caseOpener.tsx
+++ s/components/caseOpener/caseOpener.tsx
@@ -1,146 +1,84 @@
-import { useCallback, useEffect, useRef, useState } from "react"
-import { Animated, Easing, Image, Pressable, ScrollView, Text, View } from "react-native"
-import { styles } from "../../styles/caseOpenerStyles"
-import { getAuth } from "firebase/auth"
-import { caseData } from "../../interfaces/frontendInterfaces"
 import { BlurView } from "expo-blur"
 import { cases } from "../chestShop/cases"
-import { setLocalStorageData } from "../../helperFunctions/getLocalStorageData"
+import { useEffect, useRef, useState } from "react"
 import { LinearGradient } from "expo-linear-gradient"
+import { styles } from "../../styles/caseOpenerStyles"
+import { firstViewAnimate } from "./caseOpenerAnimation"
+import { openCase } from "../../userApiActions/userApiActions"
+import { caseData } from "../../interfaces/frontendInterfaces"
+import { useComponentSize } from "../../customHooks/useComponentSize"
+import { getSkinRarityFrontend } from "../../helperFunctions/getSkinRarity"
+import { Animated, Image, Pressable, ScrollView, Text, View } from "react-native"
+
 const CaseOpener = ({name, image, skins}: caseData) => {
 	const [drawnSkin, setDrawnSkin] = useState(undefined)
 	const [size, onLayout] = useComponentSize();
-	let openAnimationFirstView = useRef(new Animated.Value(500)).current
 	const [imageSize, setImageSize] = useComponentSize()
 	const [randomSkinsArray, setRandomSkinsArray] = useState([])
 	const [animationFinished, setAnimationFinished] = useState(false)
-	const auth = getAuth();
-	const firstViewAnimate = () => {
-				function getRandomInt(min, max) {
-    		min = Math.ceil(min);
-    		max = Math.floor(max);
-    		return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+	let openAnimationFirstView = useRef(new Animated.Value(500)).current
 
-	Animated.sequence([
-		Animated.timing(openAnimationFirstView, {
-			toValue: 0 - getRandomInt(0, imageSize?.width),
-			duration: 5000,
-			easing:Easing.out(Easing.ease),
-			useNativeDriver: true
-			}),
-		
-		]).start(({finished}) => {
-			if (finished) {
-				setTimeout(() => {
-						setAnimationFinished(true)
-				}, 500)
-					
-			}
-			
-		})
-	}
 	useEffect(() => {
 		if (drawnSkin) {
 			setRandomSkinsArray(getRandomSkins(drawnSkin))
 		}	
 	}, [drawnSkin])
 
-function useComponentSize() {
-  const [size, setSize] = useState(null);
-
-  const onLayout = useCallback(event => {
-    const { width, height } = event.nativeEvent.layout;
-    setSize({ width, height });
-  }, []);
-
-  return [size, onLayout];
-};
-
-
 	const thisCase = cases.filter(({ _id }) => _id == name)[0]
-	function addItemToLocalStorage(skin: string) {
-		setLocalStorageData(skin, false)
-}
-	function openCase(caseName) {
-		//141.94.85.161
-		auth.currentUser.getIdToken(/* forceRefresh */ true).then((idToken) => {
-  // Send token to your backend via HTTPS
-					fetch(`http://141.94.85.161:3000/openCase?idToken=${idToken}&caseName=${caseName}`, {
-		// fetch("http://141.94.85.161:3000/openCase", {
-						method: "GET",
-						headers: {
-				"Content-type": "application/json"
-			},
-					}).then(res => res.json()).then(data => {
-						setDrawnSkin(data), addItemToLocalStorage(data), console.log(data, "TUTAAAAAAAAAAAa")
-					})
-}).catch((error) => {
-  console.log(error)
-});
-	}
+
 	const getRandomSkins = (drawnSkin: any) => {
-		function getSkinRarity() {
-			function getRandomArbitrary() {
-    return Math.round(Math.random() * (10000 - 0) + 0)
-}
-	//losuje liczbe od 0 do 1000 i na tej podstawie daje kolor skina
-	const randomNumber = getRandomArbitrary()
-	if (randomNumber < 7992) {
-		return "Mil-Spec"
-	} else if (randomNumber > 7992 && randomNumber < 9590) {
-		return "Restricted"
-	} else if (randomNumber > 9590 && randomNumber < 9910) {
-		return "Classified"
-	} else if (randomNumber > 9910 && randomNumber < 9974) {
-		return "Covert"
-	} else if (randomNumber > 9974 && randomNumber < 10000) { 
-		return "Covert"
-	}
-	}
 		const randomSkinsArray = [...thisCase.skins].sort(() => 0.5 - Math.random())
 		for (let i = randomSkinsArray.length; i <= 34; i++) {
-			const skinQuality = getSkinRarity()
+			const skinQuality = getSkinRarityFrontend()
 			const shuffled = [...thisCase.skins].filter(({quality}) => quality == skinQuality).sort(() => 0.5 - Math.random()).slice(0, 1)
 			randomSkinsArray.push(...shuffled)
 		}
 		randomSkinsArray[2] = drawnSkin
 		const itemIndex = randomSkinsArray.indexOf(drawnSkin)
 		return randomSkinsArray
-	
 	}
+
 	const getDrawnSkinImage = () => {
 		if (drawnSkin) {
 				return skins.find(({skinName}) => drawnSkin.name.includes(skinName)).image
 			}
-		}
+	}
+
 	const renderDrawnSkin = () => {
 			return (
 			<View style={styles[`drawnSkin_${drawnSkin.rarity}`]}>
 				<Image style={styles.drawnSkinImage} source={getDrawnSkinImage()} />
 				<Text>{drawnSkin.name}</Text>
-				<Text style={styles.textContinue} onPress={() => (setDrawnSkin(undefined), setAnimationFinished(false))}>Continue</Text>
+					<Text style={styles.textContinue} onPress={() =>
+						(setDrawnSkin(undefined), setAnimationFinished(false))}>
+						Continue
+					</Text>
 			</View>
 		)
 	}
+
 	const renderSkinOpener = () => {
 			if (imageSize?.width && size?.width) {
 				openAnimationFirstView.setValue(-Math.abs(size?.width) * 8)
-				firstViewAnimate()
+				firstViewAnimate(openAnimationFirstView, imageSize?.width, setAnimationFinished)
 			}
 
 		return (
-		<BlurView intensity={80} tint={'dark'} style={{height: '100%', width: '100%', position: 'absolute', zIndex: 2, alignItems: 'center'}}>
+		<BlurView intensity={80} tint={'dark'} style={styles.blurView}>
 				<View onLayout={onLayout} style={styles.openerContainer}>
-					<View style={{ position: 'absolute', height: '100%', width: '100%', alignSelf: 'center', borderWidth: 1 , zIndex: 5, justifyContent: 'center', alignItems: 'center' }}>
-								<View style={{backgroundColor: 'red', width: 1, height: '100%'}}></View>
+					<View style={styles.middleLineContainer}>
+								<View style={styles.middleLine}></View>
 						</View>
-					<Animated.View style={{transform: [{translateX: openAnimationFirstView}], height: 70,width: '100%', zIndex: 3, borderColor: 'red', borderWidth: 1, flexDirection: 'row', position: 'absolute'}}>
+					<Animated.View style={[{transform: [{translateX: openAnimationFirstView}]}, styles.animatedView]}>
 						{randomSkinsArray?.map((item, index) => item?.image ?
-							<LinearGradient colors={['#808080', '#808080', styles[`${item.quality}`].backgroundColor]} style={[styles.openerSkinImageContainer, styles[`skinImage_${item.quality}`]]}>
+							<LinearGradient
+								colors={['#808080', '#808080', styles[`${item.quality}`].backgroundColor]}
+								style={[styles.openerSkinImageContainer, styles[`skinImage_${item.quality}`]]}>
 								<Image style={[styles.openerSkinImage]} source={item?.image} />
 							</LinearGradient>
-							: <LinearGradient colors={['#808080', '#808080', styles[`${item.rarity}`].backgroundColor]} style={[styles.openerSkinImageContainer, styles[`skinImage_${item.rarity}`]]}>
+							: <LinearGradient
+								colors={['#808080', '#808080', styles[`${item.rarity}`].backgroundColor]}
+								style={[styles.openerSkinImageContainer, styles[`skinImage_${item.rarity}`]]}>
 								<Image onLayout={setImageSize} style={[styles.openerSkinImage]} source={getDrawnSkinImage()} />
 							</LinearGradient>)}
 					</Animated.View>
@@ -151,10 +89,16 @@ function useComponentSize() {
 
 	return (
 		<View style={styles.container}>
-			{drawnSkin && !animationFinished ? renderSkinOpener() : drawnSkin && animationFinished ? renderDrawnSkin(): null}
+			{drawnSkin && !animationFinished
+				? renderSkinOpener()
+				: drawnSkin && animationFinished
+					? renderDrawnSkin()
+					: null}
 			<Image style={styles.caseImage} source={image}></Image>
-			<Pressable disabled={drawnSkin ? true : false} onPress={() => {openCase(name)}}>
-				<Text style={[styles.text, drawnSkin ? { backgroundColor: 'gray', color: 'white'} : null]}>Unlock Container</Text>
+			<Pressable disabled={drawnSkin ? true : false} onPress={() => {openCase(name, setDrawnSkin)}}>
+				<Text style={[styles.text, drawnSkin ? { backgroundColor: 'gray', color: 'white' } : null]}>
+					Unlock Container
+				</Text>
 				</Pressable>
 			<ScrollView>
 					<View style={styles.skinsContainer}>
