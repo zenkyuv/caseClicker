ccdcf44899812fe25ba558826e9990ca5cb7cc70
przemek <przemekgg2002@gmail.com>
2022-09-24 00:46:58 +0200

    refactor: simplify and cleanup code

--- s/components/profilebar.tsx
+++ s/components/profilebar.tsx
@@ -4,10 +4,14 @@ import { styles } from "../styles/profilebarStyles"
 
 const ProfileBar = () => {
 	return (
-		<BlurView intensity={80} style={styles.container}>
-			<Image style={styles.image} source={require("../images/profile-img.jpg")} />
+		<BlurView tint="dark" intensity={80} style={styles.container}>
+			<View style={styles.imageContainer}>
+				<Image style={styles.image} source={require("../images/profile-img.jpg")} />
+			</View>
 			<Image style={styles.rank} source={require("../images/silver4.png")} />
-			<View style={styles.friends}><Text style={styles.info}>i</Text></View>
+			<View style={styles.infoContainer}>
+				<Text style={styles.info}>i</Text>
+			</View>
 		</BlurView>
 	)
 }
