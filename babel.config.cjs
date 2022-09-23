9515d85b1cbc0419305a6df68db95512ce9b5557
przemek <przemekgg2002@gmail.com>
2022-09-24 01:08:35 +0200

    update: file extension js => cjs

---
+++ babel.config.cjs
@@ -0,0 +1,9 @@
+module.exports = function(api) {
+  api.cache(true);
+  return {
+		presets: ['babel-preset-expo'],
+		plugins: ['react-native-reanimated/plugin',
+			["@babel/plugin-proposal-decorators", { "version": "legacy" }],
+		["@babel/plugin-proposal-class-properties", { "loose": true }]],
+  };
+};
