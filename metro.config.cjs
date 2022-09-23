32554ce9cd679e48f5ae0a42903ca3464c9a3ece
przemek <przemekgg2002@gmail.com>
2022-09-23 05:28:54 +0200

    update: extension from js to cjs' ;

---
+++ metro.config.cjs
@@ -0,0 +1,8 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('@expo/metro-config');
+
+module.exports = getDefaultConfig(__dirname);
+
+// import {getDefaultConfig }from "@expo/metro-config"
+
+// export default getDefaultConfig(__dirname);
