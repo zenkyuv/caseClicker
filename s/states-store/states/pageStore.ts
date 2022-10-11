37a8348979581d0f218d5c213154bf10e5fe46bc
zenkyuv <przemekgg2002@gmail.com>
2022-10-11 14:24:45 +0200

    add: page actions

--- s/states-store/states/pageStore.ts
+++ s/states-store/states/pageStore.ts
@@ -5,18 +5,46 @@ import { createContext } from 'react';
 export class PageStore {
 	@observable inventoryVisible = false;
 	@observable clickerVisible = false
+	@observable chestShopVisible = false
+	@observable coinFlipVisible = false
+	@observable tradePanelVisible = false
 
   constructor() {
     makeObservable(this);
   }
 
-  @action setInventoryVisible = (bool: boolean) => {
+	@action clearState = () => {
+		this.clickerVisible = false
+		this.inventoryVisible = false
+		this.chestShopVisible = false
+		this.coinFlipVisible = false
+		this.tradePanelVisible = false
+	}
+
+	@action setInventoryVisible = (bool: boolean) => {
+		this.clearState()
     this.inventoryVisible = bool;
 	};
 	
 	@action setClickerVisible = (bool: boolean) => {
+		this.clearState()
     this.clickerVisible = bool;
-  };
+	};
+	
+	@action setChestShopVisible = (bool: boolean) => {
+		this.clearState()
+		this.chestShopVisible = bool
+	}
+
+	@action setCoinFlipVisible = (bool: boolean) => {
+		this.clearState()
+		this.coinFlipVisible = bool
+	}
+
+	@action setTradePanelVisible = (bool: boolean) => {
+		this.clearState()
+		this.tradePanelVisible = bool
+	}
 }
 const pageStore = new PageStore();
 
