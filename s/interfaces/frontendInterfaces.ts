b7790cb4c760daf91c904ca9f6aaf150be5f1950
zenkyuv <przemekgg2002@gmail.com>
2022-10-11 14:55:44 +0200

    add: frontend interfaces

---
+++ s/interfaces/frontendInterfaces.ts
@@ -0,0 +1,35 @@
+import { ImageProps } from "react-native"
+import { UserStore } from "../states-store/states/userStore"
+
+export interface UserMoney {
+	money: number
+}
+
+export interface UserData {
+	email: string,
+	password: string
+}
+
+export interface signUserInfo {
+	userData: UserData,
+	userStore: UserStore,
+	setLoadingIndicator?: any
+}
+
+interface skin {
+	weapon: string,
+	skinName: string,
+	quality: string,
+	image: any
+}
+
+export interface caseData {
+		name: string,
+		image: any,
+		skins: skin[]
+}
+
+export interface coinFlipData {
+		data: "You Lost!" | "You Won!",
+		drawnCoin: number
+	}
