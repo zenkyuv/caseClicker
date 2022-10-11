bfd4d8bbf60d9d56d10c0581813fba252f948731
zenkyuv <przemekgg2002@gmail.com>
2022-10-11 14:00:38 +0200

    style: update  profile bar styles

--- s/styles/profilebarStyles.ts
+++ s/styles/profilebarStyles.ts
@@ -3,33 +3,44 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-		backgroundColor: 'red',
+		backgroundColor: 'blue',
 		maxWidth: "10%",
+		minWidth: 50,
 		alignItems: "center",
 	},
+	imageContainer: {
+		height: 30,
+		backgroundColor: 'rgb(24, 34, 57)',
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
 		image: {
-			width: "80%",
-			height: "10%",
+			width: '60%',
+			height: '70%',
 			margin: 10
 	},
 	rank: {
 		margin: 5,
-		height: 30,
-		width: 60
+		width: '63%',
+		height: '5%',
 	},
-	friends: {
+	infoContainer: {
 		position: "absolute",
 		justifyContent: "center",
+		alignItems: 'center',
+		textAlign: 'center',
 		flex: 1,
 		height: "100%"
 	},
 	info: {
-		textAlign: "center",
-		fontSize: 20,
-		backgroundColor: "rgba(255,255,255,.6)",
-		color: "gray",
-		width: 30,
-		height: 30,
+		fontSize: 12,
+		backgroundColor: "rgba(185, 185, 185, 0.6)",
+		color: 'rgb(66, 66, 66)',
+		width: 20,
+		padding: 3,
 		borderRadius: 20,
+		alignSelf: 'center',
+		textAlign: 'center'
 	}
 });
