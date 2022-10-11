62bc00ca84d2c79ada6e52c3f0f5389b7bc68ed6
zenkyuv <przemekgg2002@gmail.com>
2022-10-11 14:48:08 +0200

    add: header styles

---
+++ s/styles/headerStyles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+export const second = (seconds) => {
+	return seconds
+}
+export const styles = StyleSheet.create({
+	container: {
+		padding: 5,
+		justifyContent: 'center',
+		height: 30,
+		backgroundColor: 'rgb(24, 34, 57)'
+	},
+	text: {
+		fontSize: 15,
+		color: 'white'
+	},
+	textGreen: {
+		fontSize: 15,
+		color: 'green'
+	}
+})
