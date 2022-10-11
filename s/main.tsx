0a0a4265c23e1b4eee231d444de79c24dc487a5c
zenkyuv <przemekgg2002@gmail.com>
2022-10-11 15:02:10 +0200

    add: new games views

--- s/main.tsx
+++ s/main.tsx
@@ -6,18 +6,32 @@ import PageStore  from "./states-store/states/pageStore";
 import { observer } from "mobx-react-lite";
 import { ImageBackground, View } from "react-native";
 import ProfileBar from "./components/profilebar";
-import Clicker from "./components/caseOpener/clicker/clicker";
+import Clicker from "./components/clicker/clicker";
+import Header from "./components/header";
+import CaseShop from "./components/caseShop/caseShop";
+import { StatusBar } from 'expo-status-bar';
+import CoinFlip from "./components/coinFlip/coinFlip";
+import TradePanel from "./components/tradePanel/tradePanel";
 
 const Mainpage = observer(() => {
 const pageStore = useContext(PageStore);
 	return (
 		<View style={styles.container}>
-				<ImageBackground source={require("./images/panorama.jpg")} style={styles.image}>
+			<StatusBar hidden={true} />
+			<ImageBackground source={require("./images/backgroundImage2.webp")} style={styles.image}>
 				<Sidenav />
-				{pageStore.inventoryVisible
-					? <Inventory />
-					: pageStore.clickerVisible
-					? <Clicker /> : null}
+				<View style={styles.flexColumn}>
+					<Header/>
+					{pageStore.inventoryVisible
+						? <Inventory />
+						: pageStore.clickerVisible
+						? <Clicker />
+						: pageStore.chestShopVisible
+						? <CaseShop />
+						: pageStore.coinFlipVisible
+						? <CoinFlip />
+						: pageStore.tradePanelVisible ? <TradePanel /> : null}
+				</View>
 				<ProfileBar />
 			</ImageBackground>
 			</View>
