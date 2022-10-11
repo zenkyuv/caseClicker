f181096df2379afc506d960f2a527de54de46603
zenkyuv <przemekgg2002@gmail.com>
2022-10-11 14:01:20 +0200

    style: update sidenav styles

--- s/styles/sidenavStyles.ts
+++ s/styles/sidenavStyles.ts
@@ -4,31 +4,50 @@ export const styles = StyleSheet.create({
   container: {
     flex: 1,
 		backgroundColor: 'red',
-		maxWidth: "10%",
+		maxWidth: "12%",
 		alignItems: "center"
 	},
 	elContainer: {
 		width: "100%",
 		alignItems: "center",
-		height: 70,
+		height: 30,
 		justifyContent: "center",
-		borderBottomWidth: 1,
-		borderBottomColor: "rgba(255,255,255,.5)"
+		backgroundColor: '#182239',
+		// borderBottomWidth: 1,
+		// borderBottomColor: "rgba(255,255,255,.5)"
 },
 	iconMain: {
 		width: 60,
 		height: 30,
 	},
 	icon: {
-		width: 30,
-		height: 30,
+		width: 25,
+		height: 25,
 		marginTop: 20,
 		marginBottom: 20,
 		// filter: invert(80 %) sepia(50%) hue-rotate(120deg),
 			
 	},
+	navTextCnt: {
+		width: '100%',
+		textAlign: 'center'
+	},
+	navText: {
+		textAlign: 'center',
+		paddingTop: 10,
+		paddingBottom: 10,
+		fontSize: 12,
+		borderBottomWidth: 1,
+		borderBottomColor: "rgba(90, 143, 210, 0.37)",
+		width: '100%',
+		color: '#5a8fd2'
+	},
 	text: {
 		color: "rgba(255,255,255,.9)",
 		fontWeight: "500",
+		fontSize: 8
+	},
+	cnt: {
+		width: '100%'
 	}
 });
