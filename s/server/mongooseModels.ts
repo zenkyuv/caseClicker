16144d93512e4e59a83dc66a74c4a19823eb737d
zenkyuv <przemekgg2002@gmail.com>
2022-10-21 01:33:52 +0200

    new file paths

--- s/server/mongooseModels.ts
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose"
-import { casesSchema, userSchema } from "./mongooseSchemas.js"
-
-export const User = mongoose.model('users', userSchema)
-export const cases = mongoose.model('cases', casesSchema)
