a515858d15511ccb98128b85259df20783babf2d
zenkyuv <przemekgg2002@gmail.com>
2022-10-21 01:23:05 +0200

    add: client websocket types

--- s/interfaces/websocketTypes.ts
+++ s/interfaces/websocketTypes.ts
@@ -1,19 +1,34 @@
 export type SessionConnectHandler = (ev: Event) => any;
-export type SessionMessageHnalder = (ev: MessageEvent<any>) => any;
+export type SessionMessageHandler = (ev: MessageEvent<any>) => any;
 export type SessionDisconnectHandler = (ev: Event) => any;
 
 export type SessionHook = [
   ConnectFN,
   <T extends any>(args: T) => void,
-  () => void
+  () => void,
 ];
 
-
-export type ConnectFN = (
+export interface RoomCreator {
+	roomID?: string,
+	action: string,
+	inventory?: any[],
+	room?: 'publicRoom' | 'privateRoom' | 'publicLobby',
+	username?: string,
+	userType: 'roomCreator',
+}
+export interface LobbyUser {
 	clientID: string,
+	connection?: WebSocket,
+	roomYouConnectedTo?: WebSocket,
+	connectedToRoom?: boolean,
+	inventory?: [],
+	userType: 'userFromLobby',
 	action: string
+}
+export type ConnectFN = (
+	data: RoomCreator | LobbyUser
 ) => void;
 
-export interface Contextable {
-  context: string;
-}
+// export interface Contextable {
+//   context: string;
+// }
