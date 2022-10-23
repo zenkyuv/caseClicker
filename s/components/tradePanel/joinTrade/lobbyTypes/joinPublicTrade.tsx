90729bfa68f3ac1b7b6f9884797671fbe3b33e9f
zenkyuv <przemekgg2002@gmail.com>
2022-10-24 00:56:59 +0200

    add: typed state

--- s/components/tradePanel/joinTrade/lobbyTypes/joinPublicTrade.tsx
+++ s/components/tradePanel/joinTrade/lobbyTypes/joinPublicTrade.tsx
@@ -2,41 +2,51 @@ import { useContext, useEffect, useState } from "react";
 import { View, Text, Image, Pressable } from "react-native"
 import UserStore from "../../../../states-store/states/userStore";
 import { useSession } from "../../../../customHooks/websocketUtils";
-import { cases } from "../../../caseShop/cases"
-import { getUsername } from "../../../../helperFunctions/localStorageFunctions";
 import { BlurView } from "expo-blur";
 import { joinPublicTradeRoom, onclose, onmessage, onopen } from "../utils/utils";
 import { getSkinsImagesFromAllConnectedUsers } from "../../../../helperFunctions/getImages";
-import TradePanel from "../../tradePanel";
 import RenderTradePanel from "../../shared/renderTradePanel";
+import { PublicRoomData } from "../../../../interfaces/frontendInterfaces";
 const JoinPublicTrade = () => {
 	const userStore = useContext(UserStore)
-	const [publicLobbies, setPublicLobbies] = useState<any>([])
-	const [connectedUserSelectedInventoryItems, setConnectedUserSelectedInventoryItems] = useState([])
+	const [publicRooms, setPublicRooms] = useState<PublicRoomData[]>()
+	const [connectedRoomData, setConnectedRoomData] = useState<PublicRoomData>()
+	const [offerAccepted, setOfferAccepted] = useState({
+		byYou: false,
+		byConnectedUser: false
+	})
 	const onOpenHandler = (data) => {onopen(data, sendMessage)}
-	const onMessageHandler = (event: MessageEvent) => {onmessage(event, setPublicLobbies, setConnectedUserSelectedInventoryItems, setUserJoined)}
+	const onMessageHandler = (event: MessageEvent) => {onmessage(event, setPublicRooms, setConnectedRoomData, setUserJoined, setOfferAccepted)}
 	const onCloseHandler = () => {onclose()}
 	const [connect, sendMessage, closeConn] = useSession(
 		onOpenHandler,onMessageHandler, onCloseHandler, userStore.userUID
 	)
 	const [userJoined, setUserJoined] = useState(false)
 	useEffect(() => {
-		connect({ action: "userJoinedPublicTradeLobby", clientID: userStore.userUID, userType: 'userFromLobby' })
+		connect({ action: "userJoinedPublicTradeLobby", clientID: userStore.userUID })
 	}, [])
-	console.log(publicLobbies)
+
 	return (
 		<>
-			{userJoined ? <RenderTradePanel connectedUserSelectedInventoryItems={connectedUserSelectedInventoryItems} userType={"userFromLobby"} userJoined={userJoined} onOpenHandler={onOpenHandler} /> : <View style={{flex: 1, margin: 10, overflow: 'hidden'}}>
-				{publicLobbies?.map(({ roomID, inventory, username }, firstIndex) =>
+			{userJoined
+				? <RenderTradePanel
+					offerAccepted={offerAccepted}
+					setOfferAccepted={setOfferAccepted}
+					data={connectedRoomData}
+					userType={"roomJoiner"}
+					userJoined={userJoined}
+					onOpenHandler={onOpenHandler} />
+				: <View style={{ flex: 1, margin: 10, overflow: 'hidden' }}>
+				{publicRooms?.map(({ roomID, selectedInventoryItems, username }, firstIndex) =>
 					<BlurView tint="dark" style={{ flexDirection: 'row', height: '20%', marginBottom: 10, zIndex: 2}} key={firstIndex}>
 						<View style={{alignItems: 'center', flex: 2}}>
 							<Image style={{width: '100%', height: '70%'}} source={require("../../../../images/profile-img.jpg")} />
 							<Text style={{color: 'white', width: '100%', height: '100%', backgroundColor: 'black', textAlign: 'center'}}>{username}</Text>
 						</View>
 						<View style={{flexDirection: 'row', flex: 5, height: '100%', alignItems: 'center', marginLeft: 10, marginRight: 10}}>
-							{inventory.map((_cos, secondIndex) => (
+							{selectedInventoryItems.map((_cos, secondIndex) => (
 								<View style={{width: '20%', height: '70%', justifyContent: 'center', alignItems: 'center'}} key={secondIndex}>
-									<Image resizeMode="center" style={{width: '100%', height: '100%'}} source={getSkinsImagesFromAllConnectedUsers(publicLobbies, firstIndex, secondIndex)} />
+									<Image resizeMode="center" style={{width: '100%', height: '100%'}} source={getSkinsImagesFromAllConnectedUsers(publicRooms, firstIndex, secondIndex)} />
 								</View>
 							))}
 						</View>
