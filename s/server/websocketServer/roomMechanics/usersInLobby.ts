be6163a4e77b07c23745abb46a76c073b4358ee5
zenkyuv <przemekgg2002@gmail.com>
2022-10-24 01:07:29 +0200

    add: more methods

--- s/server/websocketServer/roomMechanics/usersInLobby.ts
+++ s/server/websocketServer/roomMechanics/usersInLobby.ts
@@ -1,55 +1,56 @@
-// as a user in lobby you only receive changes that lobby creators do, until you joins one
-
-import { publicRoomCreators } from "./publicRoomCreators.js"
-
+// as a user in lobby you only receive events that lobby creators do, until you joins one
 // of public room, then you broadcast to that crator
+import { UserInLobby } from "../websocketModels/roomMehanicsModel.js"
+import { publicRoomCreators } from "./publicRoomCreators.js"
+import WebSocket from "ws";
 class UsersInLobby {
-	#usersInLobby = new Map<string, {
-		clientID: string,
-		connection: WebSocket,
-		roomYouConnectedTo: WebSocket,
-		connectedToRoom: boolean,
-		inventory: any
-	}>()
+	#usersInLobby = new Map<string, UserInLobby>()
 	addUserToLobby(clientID: string, userConnection: WebSocket) {
 		this.#usersInLobby.set(clientID, {
 			clientID: clientID,
 			connection: userConnection,
 			roomYouConnectedTo: undefined,
 			connectedToRoom: false,
-			inventory: []
+			selectedInventoryItems: [],
+			offerAccepted: false
 		})
+		const data = { data: publicRoomCreators.getEmptyRooms(), action: 'refreshWithNewData' }
+		userConnection.send(JSON.stringify(data))
 	}
 	getType() {
 		return this.#usersInLobby
 	}
-	getUser(clientID) {
+	getUser(clientID: string) {
 		return this.#usersInLobby.get(clientID)
 	}
-	youAreInRoom(clientID) {
+	youAreInRoom(clientID: string) {
 		return this.#usersInLobby.get(clientID).connectedToRoom
 	}
-	setRoomUserConnectedTo(clientID, roomID) {
-		const roomConnection = publicRoomCreators.getYourRoom(roomID).clientConnection
+	isOfferAccepted(clientID: string) {
+		return this.#usersInLobby.get(clientID).offerAccepted
+	}
+	setRoomUserConnectedTo(clientID: string, roomID: string) {
+		const roomConnection = publicRoomCreators.roomConnection(roomID)
 		this.#usersInLobby.set(clientID, {
 			...this.#usersInLobby.get(clientID),
 			roomYouConnectedTo: roomConnection,
 			connectedToRoom: true
 		})
 	}
-	joinPublicRoom(roomID, clientID, clientConnection) {
+	joinPublicRoom(roomID: string, clientID: string, clientConnection: WebSocket) {
 		publicRoomCreators.setUserWhoJoinedRoom(roomID, clientConnection)
 		this.setRoomUserConnectedTo(clientID, roomID)
-		publicRoomCreators.getYourRoom(roomID).clientConnection.send(JSON.stringify({ action: 'userJoinedYourRoom' }))
+		publicRoomCreators.roomConnection(roomID).send(JSON.stringify({ action: 'userJoinedYourRoom' }))
 		publicRoomCreators.broadcastMessageToEveryone(clientID, 'refreshWithNewData')
+		publicRoomCreators.broadcastMessage(roomID, 'refreshWithNewData')
 	}
 
 	getAllUsers() {
 		return [...this.#usersInLobby.values()].filter(({connectedToRoom}) => !connectedToRoom)
 	}
-	broadcastMessage(clientID) {
+	broadcastMessage(clientID: string, action: string) {
 		const client = this.#usersInLobby.get(clientID)
-		const data = { data: [...this.#usersInLobby.values()].find((client) => client.clientID == clientID), action: 'refreshWithNewData' }
+		const data = { data: [...this.#usersInLobby.values()].find((client) => client.clientID == clientID), action: action }
 		client.roomYouConnectedTo.send(JSON.stringify(data))
 	}
 }
