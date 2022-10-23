d1051d72ff700c48cb6a9404d2cd3350aec84770
zenkyuv <przemekgg2002@gmail.com>
2022-10-24 01:03:59 +0200

    add: more methods

--- s/server/websocketServer/roomMechanics/publicRoomCreators.ts
+++ s/server/websocketServer/roomMechanics/publicRoomCreators.ts
@@ -1,19 +1,14 @@
+import { PublicRoomCreator } from "../websocketModels/roomMehanicsModel.js"
 import { usersInLobby } from "./usersInLobby.js"
-
+import WebSocket from "ws"
 class PublicRoomCreators {
-	#publicRoomCreators = new Map<string, {
-		roomID: string,
-		userWhoJoinedRoom: WebSocket,
-		inventory: any,
-		clientConnection: WebSocket,
-		username: string,
-		roomEmpty: boolean,
-	}>()
-	makeRoomEmpty(roomID) {
+	#publicRoomCreators = new Map<string, PublicRoomCreator>()
+	makeRoomEmpty(roomID: string) {
 		this.#publicRoomCreators.set(roomID, {
 			...this.#publicRoomCreators.get(roomID),
 			userWhoJoinedRoom: undefined,
-			roomEmpty: true
+			roomEmpty: true,
+			offerAccepted: false
 		})
 	}
 	getType() {
@@ -22,55 +17,63 @@ class PublicRoomCreators {
 	getAllRooms() {
 		return this.#publicRoomCreators
 	}
-	getYourRoom(roomID: string) {
-		return [...this.getAllRooms().values()].find((room) => room.roomID == roomID)
+	getYourRoomData(roomID: string) {
+		return [...this.getAllRooms().values()].map(({clientConnection, userWhoJoinedRoom, ...rest}) => rest).find((room) => room.roomID == roomID)
 	}
-	getRoomCreator(clientID) {
-		return this.#publicRoomCreators.get(clientID)
+	clientConnection(roomID: string) {
+		return this.#publicRoomCreators.get(roomID).userWhoJoinedRoom
+	}
+	roomConnection(roomID: string) {
+		return this.#publicRoomCreators.get(roomID).clientConnection
+	}
+	getRoomCreator(roomID: string) {
+		return this.#publicRoomCreators.get(roomID)
 	}
 	getEmptyRooms() {
 		return [...publicRoomCreators.getAllRooms().values()].filter(({roomEmpty}) => roomEmpty).map(({clientConnection, ...rest}) => rest)
 	}
-	userHasJoinedYourRoom(client) {
+	userHasJoinedYourRoom(client: PublicRoomCreator) {
 		return client?.userWhoJoinedRoom ? true : false
 	}
-	setUserWhoJoinedRoom(roomID, clientConnection) {
+	isOfferAccepted(roomID: string) {
+		return this.#publicRoomCreators.get(roomID).offerAccepted
+	}
+	setUserWhoJoinedRoom(roomID: string, clientConnection: WebSocket) {
 		this.#publicRoomCreators.set(roomID, {
 			...this.#publicRoomCreators.get(roomID),
 			userWhoJoinedRoom: clientConnection,
 			roomEmpty: false
 		})
 	}
-	addRoomCreator(roomID, clientConnection, username) {
+	addRoomCreator(roomID: string, clientConnection: WebSocket, username: string) {
 		this.#publicRoomCreators.set(roomID, {
 			roomID: roomID,
 			userWhoJoinedRoom: undefined,
-			inventory: [],
+			selectedInventoryItems: [],
 			clientConnection: clientConnection,
 			username: username,
 			roomEmpty: true,
+			offerAccepted: false
 		})
+		publicRoomCreators.broadcastMessage(roomID, 'refreshWithNewData')
 	}
-	broadcastMessage(roomID, action) {
+	broadcastMessage(roomID: string, action: string) {
 		let client = this.#publicRoomCreators.get(roomID)
-		// if no user has joined your room then broadcast all things you do to users in lobby
 		if (this.userHasJoinedYourRoom(client)) {
-			// get only your room data instead all rooms
-			const data = {data: this.getYourRoom(roomID), action: action}
+			// get only room data from the room user joined in instead all rooms
+			const data = {data: this.getYourRoomData(roomID), action: action}
 			client.userWhoJoinedRoom.send(JSON.stringify(data))
 		} else
 		{
-			const data = { data: this.getEmptyRooms(), action: action }
-			usersInLobby.getAllUsers().forEach((client) => {
-				client.connection.send(JSON.stringify(data))
-			})
+			// if no user has joined room then broadcast all events from this room to users in lobby
+			this.broadcastMessageToEveryone(roomID, action)
 		}
 	}
-		broadcastMessageToEveryone(clientID, action) {
+		broadcastMessageToEveryone(clientID: string, action: string) {
 			const data = { data: this.getEmptyRooms(), action: action }
 			usersInLobby.getAllUsers().forEach((client) => {
 				client.connection.send(JSON.stringify(data))
-			})
+		})
 	}
 }
 
