9e349c3de6955bbed75720cf395d022138e4b051
zenkyuv <przemekgg2002@gmail.com>
2022-10-24 01:04:43 +0200

    refactor: split code

--- s/server/websocketServer/roomMechanics/userActions.ts
+++ s/server/websocketServer/roomMechanics/userActions.ts
@@ -1,50 +1,42 @@
 import { publicRoomCreators } from "./publicRoomCreators.js"
 import { usersInLobby } from "./usersInLobby.js"
-
+import { removeRoomCreator, removeRoomJoiner, roomCreatorAcceptedOffer, roomCreatorAddedItem, roomCreatorRemovedItem, roomJoinerAddedItem, roomJoinerAcceptedOffer, roomJoinerRemovedItem } from "./utils/userActionsUtils.js"
+import WebSocket from "ws";
+import { Item } from "../../../interfaces/sharedInterfaces.js";
 class UserActions {
-	removeItem(clientID, indexToRemove, type) {
-		let client = type.get(clientID)
-		const clientInventory = [...client.inventory]
-		clientInventory.splice(indexToRemove, 1)
-		type.set(clientID, {
-			...type.get(clientID),
-			inventory: clientInventory,
-		})
+	removeItem(clientID: string, indexToRemove: number, userType: string) {
+		if (userType == 'roomCreator') {
+			roomCreatorRemovedItem(clientID, indexToRemove)
+		}
+		if (userType == 'roomJoiner') {
+			roomJoinerRemovedItem(clientID, indexToRemove)
+		}
 	}
-
-	addItem(clientID, selectedItem, type) {
-		const client = type.get(clientID)
-		const clientInventory = [...client.inventory]
-		clientInventory.push(selectedItem)
-		type.set(clientID, {
-			...type.get(clientID),
-			inventory: clientInventory,
-		})
+	addItem(clientID: string, selectedItem: Item, userType: string) {
+		if (userType == 'roomCreator') {
+			roomCreatorAddedItem(clientID, selectedItem)
+		}
+		if (userType == 'roomJoiner') {
+			roomJoinerAddedItem(clientID, selectedItem)
+		}
 	}
-
-	removeUser(websocketConnection) {
-		const lobbyUser = [...usersInLobby.getType().values()].find(({ connection }) => connection == websocketConnection)
+	userAcceptedOffer(userType: string, clientID: string, websocketConnection: WebSocket) {
+		if (userType == 'roomCreator') {
+			roomCreatorAcceptedOffer(clientID, websocketConnection)
+		}
+		if (userType == 'roomJoiner') {
+			roomJoinerAcceptedOffer(clientID, websocketConnection)
+		}
+	}
+	removeUser(websocketConnection: WebSocket) {
+		// find whether its room joiner or room creator to be removed based on websocket conenction
+		const roomJoiner = [...usersInLobby.getType().values()].find(({ connection }) => connection == websocketConnection)
 		const roomCreator = [...publicRoomCreators.getType().values()].find(({ clientConnection }) => clientConnection == websocketConnection)
-		if (lobbyUser) {
-			const clientID = lobbyUser.clientID
-			const roomID = [...publicRoomCreators.getType().values()].find(({ clientConnection }) => clientConnection == lobbyUser.roomYouConnectedTo).roomID
-			lobbyUser.roomYouConnectedTo.send(JSON.stringify({ action: "userLeftRoom" }))
-			usersInLobby.getType().delete(clientID)
-			publicRoomCreators.makeRoomEmpty(roomID)
-			publicRoomCreators.broadcastMessage(roomID, 'refreshWithNewData')
+		if (roomJoiner) {
+			removeRoomJoiner(roomJoiner)
 		} 
 		if (roomCreator) {
-			const roomID = roomCreator.roomID
-			const data = { data: publicRoomCreators.getEmptyRooms(), action: "userLeftRoom" }
-			if (roomCreator.roomEmpty) {
-				publicRoomCreators.getType().delete(roomID)
-				publicRoomCreators.broadcastMessage(roomID, 'userLeftRoom')
-			}
-			if (!roomCreator.roomEmpty) {
-				const userWhoJoinedRoom = publicRoomCreators.getYourRoom(roomID).userWhoJoinedRoom
-				publicRoomCreators.getType().delete(roomID)
-				userWhoJoinedRoom.send(JSON.stringify(data))
-			}
+			removeRoomCreator(roomCreator)
 		}
 	}
 }
